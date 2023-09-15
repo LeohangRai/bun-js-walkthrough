44d6753d48a62e46c1010dba2a52ee6abe9dd167
Anup Rai <anup.rai@ekbana.info>
2023-09-15 22:33:45 +0545

    style: update spacing and styling

--- index.ts
+++ index.ts
@@ -1,27 +1,27 @@
 import figlet from 'figlet';
 import helloText from './hello.txt';
 
-const PORT = process.env.PORT ?? 3000
+const PORT = process.env.PORT ?? 3000;
 
 const server = Bun.serve({
-    port: PORT,
-    fetch(req: Request) {
-        const url = new URL(req.url);
-        switch(url.pathname) {
-            case '/':
-            case '/home':
-                return new Response(figlet.textSync(helloText));
-            case '/about':
-                return new Response(figlet.textSync('About Page'));
-            case '/file':
-                return new Response(Bun.file('./animal-farm-orwell.txt'));
-            case '/video':                
-                return new Response(Bun.file('./video.mp4').stream())
-            default:
-                return new Response(figlet.textSync('404 NOT FOUND'));
-        }
+  port: PORT,
+  fetch(req: Request) {
+    const url = new URL(req.url);
+    switch (url.pathname) {
+      case '/':
+      case '/home':
+        return new Response(figlet.textSync(helloText));
+      case '/about':
+        return new Response(figlet.textSync('About Page'));
+      case '/file':
+        return new Response(Bun.file('./animal-farm-orwell.txt'));
+      case '/video':
+        return new Response(Bun.file('./video.mp4').stream());
+      default:
+        return new Response(figlet.textSync('404 NOT FOUND'));
     }
+  }
 });
 
 console.log(figlet.textSync(`BUN ${Bun.version}`));
-console.log("Server is up and running at PORT:", PORT);
+console.log('Server is up and running at PORT:', PORT);
