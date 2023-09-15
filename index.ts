cf2eefe8e5df825d9b4c6caa3915819abd88ce78
Anup Rai <anup.rai@ekbana.info>
2023-09-15 22:08:49 +0545

    feat: routing with bun.server() and handing file streams

--- index.ts
+++ index.ts
@@ -5,8 +5,21 @@ const PORT = process.env.PORT ?? 3000
 
 const server = Bun.serve({
     port: PORT,
-    fetch() {
-        return new Response(figlet.textSync(helloText));
+    fetch(req: Request) {
+        const url = new URL(req.url);
+        switch(url.pathname) {
+            case '/':
+            case '/home':
+                return new Response(figlet.textSync(helloText));
+            case '/about':
+                return new Response(figlet.textSync('About Page'));
+            case '/file':
+                return new Response(Bun.file('./animal-farm-orwell.txt'));
+            case '/video':                
+                return new Response(Bun.file('./video.mp4').stream())
+            default:
+                return new Response(figlet.textSync('404 NOT FOUND'));
+        }
     }
 });
 
